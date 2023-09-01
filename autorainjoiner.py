ddc5e6ea4298480bd3189a1d84dd37f757ae25da
epicnftmaker <143820156+epicnftmaker@users.noreply.github.com>
2023-09-02 00:10:27 +0930

    Create autorainjoiner.py

---
+++ autorainjoiner.py
@@ -0,0 +1,32 @@
+import discord
+import aiohttp
+import asyncio
+from discord.ext import commands
+import random
+import string
+from dhooks import Webhook
+
+
+bot1 = commands.Bot(command_prefix='!', self_bot=True)
+
+
+bot2 = commands.Bot(command_prefix='!', self_bot=True)
+
+@bot1.event
+async def on_message(message: discord.Message):    
+    ch = await bot1.fetch_channel(1126511960545046659)
+    async for message in ch.history(limit=1):
+        for comp in message.components:
+            for child in comp.children:
+                await child.click()
+                
+@bot2.event
+async def on_message(message: discord.Message):    
+    ch = await bot1.fetch_channel(1126511960545046659)
+    async for message in ch.history(limit=1):
+        for comp in message.components:
+            for child in comp.children:
+                await child.click()
+
+
+bot1.run('MTEzMTUxMzUxNzg1NDQ0MTU3Mg.GnnqRK.bGN9yrYNLAB9y0g1dynmeTYYYHKBGGvW327s6g')
