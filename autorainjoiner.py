4f0f06166aff93555a188187d0d73559cf1b0545
epicnftmaker <143820156+epicnftmaker@users.noreply.github.com>
2023-09-03 01:38:30 +0930

    Update autorainjoiner.py

--- autorainjoiner.py
+++ autorainjoiner.py
@@ -10,7 +10,8 @@ from dhooks import Webhook
 bot1 = commands.Bot(command_prefix='!', self_bot=True)
 
 
-bot2 = commands.Bot(command_prefix='!', self_bot=True)
+# psxbet auto rain joiner 
+
 
 @bot1.event
 async def on_message(message: discord.Message):    
@@ -20,13 +21,7 @@ async def on_message(message: discord.Message):
             for child in comp.children:
                 await child.click()
                 
-@bot2.event
-async def on_message(message: discord.Message):    
-    ch = await bot1.fetch_channel(1126511960545046659)
-    async for message in ch.history(limit=1):
-        for comp in message.components:
-            for child in comp.children:
-                await child.click()
 
 
-bot1.run('MTEzMTUxMzUxNzg1NDQ0MTU3Mg.GnnqRK.bGN9yrYNLAB9y0g1dynmeTYYYHKBGGvW327s6g')
+
+bot1.run('')
